7340509da5832fdad76fa4f1f5ddf6a06bd8c71f
Suman Das <srisumandas66@gmail.com>
2025-10-16 14:50:18 +0530

    updated config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import dotenv from 'dotenv';
+import { defineConfig } from 'vite';
+
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/',
   server: {
     proxy: {
       '/api': {
